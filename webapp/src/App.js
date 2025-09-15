f37641aa294ba917974a905184726102823e2af7
bhuvanbhaskar9876 <shashimatho25@gmail.com>
2025-09-15 21:31:57 +0530

    Add App.js for React web app

---
+++ webapp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{textAlign: "center", marginTop: "100px"}}>
+      <h1>Welcome to Bhuvanmagic!</h1>
+      <p>This is your starter web app. Edit <code>src/App.js</code> to change this page.</p>
+    </div>
+  );
+}
+
+export default App;
